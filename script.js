b7197860858a54873fbd187b88bca7e2d3ddd303
Khushi1524 <khushitiwari2415@gmail.com>
2025-01-18 22:31:54 +0530

    added menu responsivness

---
+++ script.js
@@ -0,0 +1,10 @@
+const menuList = document.querySelector("#menuList");
+const menuClose = document.querySelector('#menuClose');
+
+function toggleMenu(){
+ menuList.style.display = "block";
+}
+
+function menuClosebtn(){
+    menuList.style.display = "none"; 
+}
